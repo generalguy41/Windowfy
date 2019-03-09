22b51eea58a4e299206d8772a460917523180aa8
Sebastian Guy <generalguy_41@outlook.com>
2019-03-09 11:50:26 -0400

    Clean up after merge conflicts
    Signed-off-by: Sebastian Guy <generalguy_41@outlook.com>

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import { sites } from './sites.js'
@@ -6,9 +5,6 @@ import { sites } from './sites.js'
 var isFirefox = typeof InstallTrigger !== "undefined";
 var isIE = /*@cc_on!@*/ false || !!document.documentMode;
 // var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
-=======
-var isIE = /* @cc_on!@ */ false || !!document.documentMode;
->>>>>>> 5c60690f5f077ae9692a3e16984e9e4f16c6f3aa
 var isEdge = !isIE && !!window.StyleMedia;
 var isChrome = !!window.chrome && !!window.chrome.webstore;
 
@@ -29,7 +25,6 @@ browserOrChrome().contextMenus.create({
     onclick: windowfy
 });
 
-<<<<<<< HEAD
 function windowfy() {
     browserOrChrome().tabs.executeScript({
         code: `let SITES = ${JSON.stringify(sites)};`,
@@ -39,17 +34,3 @@ function windowfy() {
         file: "scripts/windowfy.js",
     });
 }
-=======
-function expand() {
-  // browser.storage.local.get("state").then((data)=> {
-  //   console.log(data.state.expanded)
-  //   if ((data.state.expanded === false) || (data.state.expanded === null)){
-  //     var state = {
-  //       expanded: true
-  //     }
-  //     browser.storage.local.set({state});
-  browserOrChrome().tabs.executeScript({
-    file: "scripts/expand.js"
-  });
-}
->>>>>>> 5c60690f5f077ae9692a3e16984e9e4f16c6f3aa
